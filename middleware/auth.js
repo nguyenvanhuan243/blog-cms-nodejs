9d53410069b6246156c926056143cbb52964a157
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-04-17 12:03:34 +0700

    format code

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,54 +1,54 @@
 var middlewareObj = {},
-    Comment = require('../models/comment'),
-    Post = require('../models/post');
+	Comment = require('../models/comment'),
+	Post = require('../models/post');
 
-middlewareObj.checkLoggedIn = function(req, res, next){
-    if(req.isAuthenticated()){
-        return next();
-    }
-    req.flash("error", "You must Be logged In to do that")
-    res.redirect('/login')
+middlewareObj.checkLoggedIn = function (req, res, next) {
+	if (req.isAuthenticated()) {
+		return next();
+	}
+	req.flash("error", "You must Be logged In to do that")
+	res.redirect('/login')
 };
 
-middlewareObj.checkCommentOwnership = function(req, res, next){
-    if(req.isAuthenticated()){
-        Comment.findById(req.params.commentId, function(err, foundComment){
-            if(err){
-                console.log(err)
-            }else{
-                //Check if user is authenticated
-                if(foundComment.author.id.equals(req.user._id)){
-                    next();
-                }else{
-                    res.send("You are Not authorised To edit the page")
-                }
-            }
-        })
-    }else{
-        req.flash("error", "You must Be logged In to do that")
-        res.redirect("back")
-    }
+middlewareObj.checkCommentOwnership = function (req, res, next) {
+	if (req.isAuthenticated()) {
+		Comment.findById(req.params.commentId, function (err, foundComment) {
+			if (err) {
+				console.log(err)
+			} else {
+				//Check if user is authenticated
+				if (foundComment.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.send("You are Not authorised To edit the page")
+				}
+			}
+		})
+	} else {
+		req.flash("error", "You must Be logged In to do that")
+		res.redirect("back")
+	}
 }
 
 
-middlewareObj.checkPostOwnership = function(req, res, next){
-    if(req.isAuthenticated()){
-        Post.findById(req.params.id, function(err, foundPost){
-            if(err){
-                console.log(err)
-            }else{
-                //Check if user is authenticated
-                if(foundPost.author.id.equals(req.user._id)){
-                    next();
-                }else{
-                    res.send("You are Not authorised To edit the page")
-                }
-            }
-        })
-    }else{
-        req.flash("error", "You must Be logged In to do that")
-        res.redirect("back")
-    }
+middlewareObj.checkPostOwnership = function (req, res, next) {
+	if (req.isAuthenticated()) {
+		Post.findById(req.params.id, function (err, foundPost) {
+			if (err) {
+				console.log(err)
+			} else {
+				//Check if user is authenticated
+				if (foundPost.author.id.equals(req.user._id)) {
+					next();
+				} else {
+					res.send("You are Not authorised To edit the page")
+				}
+			}
+		})
+	} else {
+		req.flash("error", "You must Be logged In to do that")
+		res.redirect("back")
+	}
 }
 
 
