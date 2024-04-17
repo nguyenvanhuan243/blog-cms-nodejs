f13837ffd1333c56ac7340a390378272c5b9d0e2
Huan Nguyen <nguyenvanhuan243@gmail.com>
2024-04-17 12:08:43 +0700

    format auth router

--- routes/auth.js
+++ routes/auth.js
@@ -1,39 +1,37 @@
-var express = require('express'),
-    router = express.Router(),
-    User = require('../models/user'),
-    passport = require("passport"),
-    localStrategy = require("passport-local"),
-    passportLocalMongoose = require("passport-local-mongoose");
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+const passport = require("passport")
 
-router.get('/login', function(req, res){
-    res.render("auth/login")
+router.get('/login', function (req, res) {
+	res.render("auth/login")
 });
 
-router.get('/register', function(req, res){
-    res.render("auth/register")
+router.get('/register', function (req, res) {
+	res.render("auth/register")
 })
 
-router.post('/register', function(req, res){
-    User.register(new User({username: req.body.username}), req.body.password, function(err, msg){
-        if(err){
-            console.log(err)
-        }
-        passport.authenticate('/local')(req, res, function(){
-            res.redirect("/")
-        })
-    })
+router.post('/register', function (req, res) {
+	User.register(new User({ username: req.body.username }), req.body.password, function (err, msg) {
+		if (err) {
+			console.log(err)
+		}
+		passport.authenticate('/local')(req, res, function () {
+			res.redirect("/")
+		})
+	})
 })
 
 router.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login'
-}), function(req, res){
-    //Nothing Here
+	successRedirect: '/',
+	failureRedirect: '/login'
+}), function (req, res) {
+	//Nothing Here
 })
 
-router.get('/logout', function(req, res){
-    req.logout();
-    res.redirect('/')
+router.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/')
 })
 
 module.exports = router;
